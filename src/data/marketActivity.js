6aeff8c97266f4f6e398c271a6b15eee9edee299
realtorharishp <realtor.harishp@gmail.com>
2026-05-17 07:54:55 -0500

    commit

---
+++ src/data/marketActivity.js
@@ -0,0 +1,22 @@
+export const listings = [
+  {
+    number: 1,
+    status: 'Residential Sale',
+    title: '14839 Story Lane',
+    city: 'Frisco, TX 75035',
+    price: '$923,631',
+    image: '/listings/14839-story-lane/story-1.jpg',
+    facts: ['4 Bed', '3.5 Bath', '3,593 Sq Ft', 'Built 2011'],
+    link: '/listings/14839-story-lane'
+  },
+  {
+    number: 2,
+    status: 'Residential Sale',
+    title: '9650 Amberwoods Lane',
+    city: 'Frisco, TX',
+    price: '$549,999',
+    image: '/listings/9650-amberwoods-lane/amber-1.jpg',
+    facts: ['3 Bed', '2.5 Bath', '2,593 Sq Ft', 'Built 2013'],
+    link: '/listings/9650-amberwoods-lane'
+  }
+]
