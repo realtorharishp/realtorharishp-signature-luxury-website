69f89274e07fb668f6fffc19c46c2af3e408b91c
realtorharishp <realtor.harishp@gmail.com>
2026-05-17 07:59:48 -0500

    Update marketActivity.js

--- src/data/marketActivity.js
+++ src/data/marketActivity.js
@@ -1,22 +1,75 @@
-export const listings = [
+export const marketSections = [
   {
-    number: 1,
-    status: 'Residential Sale',
-    title: '14839 Story Lane',
-    city: 'Frisco, TX 75035',
-    price: '$923,631',
-    image: '/listings/14839-story-lane/story-1.jpg',
-    facts: ['4 Bed', '3.5 Bath', '3,593 Sq Ft', 'Built 2011'],
-    link: '/listings/14839-story-lane'
+    title: 'Upcoming Open Houses',
+    items: [
+        {
+          address: '14839 Story Lane',
+          city: 'Frisco, TX, 75035',
+          price: '$923,631',
+          image: '/listings/14839-story-lane/story-1.jpg',
+          status: 'Open House • Saturday 23rd May, 2026 between 2PM–5PM'
+        },
+        {
+          address: '9650 Amberwoods Lane',
+          city: 'Frisco, TX, 75035',
+          price: '$549,999',
+          image: '/listings/9650-amberwoods-lane/amber-1.jpg',
+          status: 'Open House • Sunday 24th May, 2026 between 2PM–5PM'
+        },
+        {
+          address: '12820 Cooper River Trail',
+          city: 'Frisco, TX 75035',
+          price: '$865,000',
+          image: '/cooper-river.jpg',
+          status: 'Open House • Sunday 17th May, 2026 between 2PM–4PM'
+        }
+      ]
   },
+
   {
-    number: 2,
-    status: 'Residential Sale',
-    title: '9650 Amberwoods Lane',
-    city: 'Frisco, TX',
-    price: '$549,999',
-    image: '/listings/9650-amberwoods-lane/amber-1.jpg',
-    facts: ['3 Bed', '2.5 Bath', '2,593 Sq Ft', 'Built 2013'],
-    link: '/listings/9650-amberwoods-lane'
+    title: 'Under Contract',
+    items: [
+        {
+          address: '2761 Douglas Lane',
+          city: 'Prosper, TX',
+          price: '$1,041,000',
+          image: '/listings/douglas.jpg',
+          status: 'Under Contract'
+        },
+        {
+          address: '2661 Archgate CT',
+          city: 'Prosper, TX',
+          price: '$1,300,100',
+          image: '/listings/archgate.jpg',
+          status: 'Under Contract'
+        },
+        {
+          address: '4433 Ginger Road',
+          city: 'Prosper, TX',
+          price: '$670,000',
+          image: '/listings/ginger.jpg',
+          status: 'Under Contract'
+        }
+      ]
+  },
+
+  {
+    title: 'Recently Closed',
+     items: [
+        {
+          address: '5109 Fort Bucker Drive',
+          city: 'Mckinney, TX',
+          price: '$605,000',
+          image: '/listings/fort-buckner.jpg',
+          status: 'Sold'
+        },
+        {
+          address: '2560 Valley Glen Drive',
+          city: 'Little Elm, TX',
+          price: '$3250',
+          image: '/listings/valley.jpg',
+          status: 'Sold'
+        }
+      ]
   }
 ]
