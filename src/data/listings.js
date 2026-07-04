d2bdd25e434da0fa9c4f98b207c052c65ee9c17f
realtorharishp <realtor.harishp@gmail.com>
2026-07-04 17:45:04 -0500

    Update listings.js

--- src/data/listings.js
+++ src/data/listings.js
@@ -15,7 +15,7 @@ export const listings = [
   status: 'Residential Lease Or Sale',
   title: '2201 Peters Colony Drive',
   city: 'Little Elm, Texas 75036',
-  price: '$599,999',
+  price: '$572,999',
   image: '/listings/peters/peters-1.jpg',
   gallery: Array.from(
     { length: 15 },
