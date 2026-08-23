905d90e4c7791e056cd85331f03bdfa09f83e4dd
realtorharishp <realtor.harishp@gmail.com>
2026-08-23 05:46:07 -0500

    Update listings.js
    Commit

--- src/data/listings.js
+++ src/data/listings.js
@@ -15,8 +15,8 @@ export const listings = [
   status: 'Residential Lease Or Sale',
   title: '754 Corner Post Path',
   city: 'Celina, Texas 75009',
-  price: '$572,999',
-  image: '/listings/peters/peters-1.jpg',
+  price: '$573,999',
+  image: '/listings/corner/corner-1.jpg',
   gallery: Array.from(
     { length: 25 },
     (_, i) => `/listings/corner/corner-${i + 1}.jpg`
@@ -88,17 +88,6 @@ export const listings = [
 
 
   {
-    slug: '9650-amberwoods-lane',
-    status: 'Residential Sale',
-    title: '9650 Amberwoods Lane',
-    city: 'Frisco, TX',
-    price: '$520,999',
-    image: '/listings/9650-amberwoods-lane/amber-1.jpg',
-    facts: ['3 Bed', '2.5 Bath', '2,593 Sq Ft', 'Built 2013'],
-    link: '/listings/9650-amberwoods-lane'
-  },
-
-  {
     slug: '2935-shetland-drive',
     status: 'Residential Sale',
     title: '2935 Shetland Drive',
@@ -132,42 +121,7 @@ export const listings = [
     link: '/listings/2900-galveston'
   },
 
-  {
-   slug: '1009-village-wood-ct',
-    status: 'Residential Lease',
-    title: '1009 Village Wood Ct',
-    city: 'Arlington, TX',
-    price: '$2,850 / Month',
-    image: '/listings/1009-village-wood-ct/village-1.jpg',
-    facts: ['4 Bed', '2.5 Bath', '2,336 Sq Ft', 'Built 1987'],
-    link: '/listings/1009-village-wood-ct'
-  },
 
-  {
-  slug: 'cooper',
-  status: 'Residential Sale',
-  title: '12820 Cooper River Trail',
-  city: 'Frisco, Texas 75035',
-  price: '$865,000',
-  image: '/listings/cooper/cooper-1.jpg',
-  gallery: Array.from(
-    { length: 20 },
-    (_, i) => `/listings/cooper/cooper-${i + 1}.jpg`
-  ),
-  facts: ['4 Bed', '2.5 Bath', '3,385 Sq Ft', 'Built 2020'],
-  link: '/listings/cooper'
-},
-
-  {
-    slug: '615-hemmingway-lane',
-    status: 'Residential Lease',
-    title: '615 Hemmingway Lane',
-    city: 'McKinney, TX',
-    price: '$2,239 / Month',
-    image: '/listings/615-hemmingway-lane/hemmingway-1.jpg',
-    facts: ['4 Bed', '2 Bath', '1,605 Sq Ft', 'Built 2024'],
-    link: '/listings/615-hemmingway-lane'
-  },
 
   {
     slug: '4414-silverweed-lane',
@@ -190,20 +144,6 @@ export const listings = [
     facts: ['5 Bed', '3 Bath', '2,212 Sq Ft', 'Built 2024'],
     link: '/listings/1874-purtis-creek'
   },
-  {
-  slug: 'arrow',
-  status: 'Residential Sale',
-  title: '1312 Arrowwood Drive',
-  city: 'Aubrey, Texas 76227',
-  price: '$549,999',
-  image: '/listings/arrow/arrow-1.jpg',
-  gallery: Array.from(
-    { length: 15 },
-    (_, i) => `/listings/arrow/arrow-${i + 1}.jpg`
-  ),
-  facts: ['4 Bed', '3.5 Bath', '3,106 Sq Ft', 'Built 2021'],
-  link: '/listings/arrow'
-},
 
    {
    slug: '400-stonebrook-301',
