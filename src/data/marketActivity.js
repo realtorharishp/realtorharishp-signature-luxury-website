aad06927f5a36e0f4324f4eb3ea6e29b95adc241
realtorharishp <realtor.harishp@gmail.com>
2026-08-23 05:27:16 -0500

    Update marketActivity.js

--- src/data/marketActivity.js
+++ src/data/marketActivity.js
@@ -6,50 +6,26 @@ export const marketSections = [
       {
           address: '625 Yarrow Street',
           city: 'Little Elm, Texas 75068',
-          price: '$774,599',
+          price: '$744,599',
           image: '/listings/Yarrow/Yarrow-1.jpg',
-          status: 'Open House • Sunday 5th July, 2026 between 1PM–3PM'
+          status: 'Open House • Sunday 6th Sept, 2026 between 1PM–4PM'
         }, 
 
-      {
-          address: '14839 Story Lane',
-          city: 'Frisco, TX, 75035',
-          price: '$839,999',
-          image: '/listings/14839-story-lane/story-1.jpg',
-          status: 'Open House • Sunday 5th July, 2026 between 2PM–5PM'
-        },
         {
-          address: '9650 Amberwoods Lane',
-          city: 'Frisco, TX, 75035',
-          price: '$572,999',
-          image: '/listings/9650-amberwoods-lane/amber-1.jpg',
-          status: 'Open House • Sunday 5th July, 2026 between 11:30AM–1:30PM'
+          address: '754 Corner Post Path',
+          city: 'Celina, TX, 75009',
+          price: '$573,999',
+          image: '/listings/corner/corner-1.jpg',
+          status: 'Open House • Sat 5th Sept, 2026 between 1:00PM–4:00PM'
         },
-
-      {
-          address: '2201 Peters Colony Drive',
-          city: 'Little Elm, Texas 75068',
-          price: '$520,000',
-          image: '/listings/peters/peters-1.jpg',
-          status: 'Open House • Sunday 5th July, 2026 between 10AM-1PM'
-        }, 
-      
-      {
-          address: '1312 Arrowwood Drive',
-          city: 'Aubrey, Texas 76227',
-          price: '$520,000',
-          image: '/listings/arrow/arrow-1.jpg',
-          status: 'Open House • Sunday 5th July, 2026 between 1PM–3PM'
-        }, 
-     
       
        
        {
-          address: '12820 Cooper River Trail',
-          city: 'Frisco, TX 75035',
-          price: '$865,000',
-          image: '/cooper-river.jpg',
-          status: 'Open House • Sunday 5th July, 2026 between 1PM–3PM'
+          address: '6451 Still Waters Court',
+          city: 'Midlothian, Texas 76065',
+          price: '$729,999',
+          image: '/stillwater.jpg',
+          status: 'Open House • Sat 29th Aug, 2026 between 1PM–4PM'
         }  
       ]
   },
@@ -57,33 +33,12 @@ export const marketSections = [
   {
     title: 'Under Contract',
     items: [
-        {
-          address: '2761 Douglas Lane',
-          city: 'Prosper, TX',
-          price: '$1,041,000',
-          image: '/listings/douglas.jpg',
-          status: 'Under Contract'
-        },
+        
         {
           address: '2661 Archgate CT',
           city: 'Prosper, TX',
           price: '$1,300,100',
           image: '/listings/archgate.jpg',
-          status: 'Under Contract'
-        },
-        {
-          address: '6007 Alderton Drive',
-          city: 'Mckinney, TX',
-          price: '$363,000',
-          image: '/listings/alderton/alderton-1.jpg',
-          status: 'Under Contract'
-        },
-
-      {
-          address: '1418 Old Stead Drive',
-          city: 'Mckinney, TX',
-          price: '$359,999',
-          image: '/listings/oldstead/oldstead-1.jpg',
           status: 'Under Contract'
         }
       
@@ -111,36 +66,42 @@ export const marketSections = [
         },
 
         {
-          address: '717 Dotty Drive',
+          address: '6204 Emery Hill Drive',
           city: 'Mckinney, TX',
-          price: '$3200',
+          price: '$359.999',
           image: '/listings/dotty/dotty-1.jpg',
-          status: 'Leased'
+          status: 'Sold'
         },
        
         {
-          address: '2560 Valley Glen Drive',
-          city: 'Little Elm, TX',
-          price: '$3250',
-          image: '/listings/valley.jpg',
+          address: '2761 Douglas Lane',
+          city: 'Prosper, TX',
+          price: '$1,041,000',
+          image: '/listings/douglas.jpg',
+          status: 'Sold'
+        },
+        {
+          address: '9650 Amberwood Lane',
+          city: 'Frisco, TX, 75035',
+          price: '$520,999',
+          image: '/listings/9650-amberwoods-lane/amber-1.jpg',
+          status: 'Under Contract'
+        },
+        {
+          address: '6007 Alderton Drive',
+          city: 'Mckinney, TX',
+          price: '$363,000',
+          image: '/listings/alderton/alderton-1.jpg',
           status: 'Sold'
         },
 
-       {
-          address: '615 Hemmingway Lane',
+      {
+          address: '1418 Old Stead Drive',
           city: 'Mckinney, TX',
-          price: '$2,650',
-          image: '/listings/615-hemmingway-lane/hemmingway-1.jpg',
-          status: 'Leased'
-        },
-       
-        {
-          address: '2900 Galveston Street',
-          city: 'Plano, TX',
-          price: '$2,850',
-          image: '/listings/2900-galveston/galveston-1.jpg',
-          status: 'Leased'
-        },
+          price: '$359,999',
+          image: '/listings/oldstead/oldstead-1.jpg',
+          status: 'Sold'
+        }
        
        {
           address: '4433 Ginger Road',
