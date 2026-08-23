eaff1cd42fd05a0bde9d114fee680fb1eb6ebe89
realtorharishp <realtor.harishp@gmail.com>
2026-08-23 06:28:54 -0500

    Update listings.js

--- src/data/listings.js
+++ src/data/listings.js
@@ -1,91 +1,63 @@
 export const listings = [
   {
-    slug: '14839 Story Lane',
+    slug: 'corner',
+    status: 'Residential Lease Or Sale',
+    title: '754 Corner Post Path',
+    city: 'Celina, Texas 75009',
+    price: '$573,999',
+    image: '/listings/corner/corner-1.jpg',
+    gallery: Array.from(
+      { length: 25 },
+      (_, i) => `/listings/corner/corner-${i + 1}.jpg`
+    ),
+    facts: ['4 Bed', '3 Bath', '3,008 Sq Ft', 'Built 2020'],
+    link: '/listings/corner/corner'
+  },
+
+  {
+    slug: '14839-story-lane',
     status: 'Residential Lease',
     title: '14839 Story Lane',
     city: 'Frisco, TX 75035',
-    price: '$3899',
+    price: '$3,899 / Month',
     image: '/listings/14839-story-lane/story-1.jpg',
+    gallery: Array.from(
+      { length: 20 },
+      (_, i) => `/listings/14839-story-lane/story-${i + 1}.jpg`
+    ),
     facts: ['4 Bed', '3.5 Bath', '3,593 Sq Ft', 'Built 2011'],
     link: '/listings/14839-story-lane'
   },
 
   {
-  slug: 'corner',
-  status: 'Residential Lease Or Sale',
-  title: '754 Corner Post Path',
-  city: 'Celina, Texas 75009',
-  price: '$573,999',
-  image: '/listings/corner/corner-1.jpg',
-  gallery: Array.from(
-    { length: 25 },
-    (_, i) => `/listings/corner/corner-${i + 1}.jpg`
-  ),
-  facts: ['4 Bed', '3 Bath', '3,008 Sq Ft', 'Built 2020'],
-  link: '/listings/corner/corner'
-},
-  
- {
-  slug: 'Yarrow',
-  status: 'Residential Sale',
-  title: '625 Yarrow Street',
-  city: 'Little Elm, Texas 75068',
-  price: '$774,999',
-  image: '/listings/Yarrow/Yarrow-1.jpg',
-  gallery: Array.from(
-    { length: 15 },
-    (_, i) => `/listings/Yarrow/Yarrow-${i + 1}.jpg`
-  ),
-  facts: ['5 Bed', '4 Bath', '4,070 Sq Ft', 'Built 2018'],
-  link: '/listings/Yarrow/Yarrow'
-},
-  
- {
-  slug: 'dotty',
-  status: 'Residential Lease Or Sale',
-  title: '717 Dotty Drive',
-  city: 'Mckinney, Texas 75071',
-  price: '$3200',
-  image: '/listings/dotty/dotty-1.jpg',
-  gallery: Array.from(
-    { length: 15 },
-    (_, i) => `/listings/dotty/dotty-${i + 1}.jpg`
-  ),
-  facts: ['4 Bed', '2.5 Bath', '2,337 Sq Ft', 'Built 2026'],
-  link: '/listings/dotty/dotty'
-},
-
- {
-  slug: 'alderton',
-  status: 'Residential Lease Or Sale',
-  title: '6007 Alderton drive',
-  city: 'Mckinney, Texas 75071',
-  price: '$3200',
-  image: '/listings/alderton/alderton-1.jpg',
-  gallery: Array.from(
-    { length: 15 },
-    (_, i) => `/listings/alderton/alderton-${i + 1}.jpg`
-  ),
-  facts: ['4 Bed', '2.5 Bath', '2,337 Sq Ft', 'Built 2026'],
-  link: '/listings/alderton/alderton'
-},
-  
+    slug: 'Yarrow',
+    status: 'Residential Sale',
+    title: '625 Yarrow Street',
+    city: 'Little Elm, Texas 75068',
+    price: '$774,999',
+    image: '/listings/Yarrow/Yarrow-1.jpg',
+    gallery: Array.from(
+      { length: 15 },
+      (_, i) => `/listings/Yarrow/Yarrow-${i + 1}.jpg`
+    ),
+    facts: ['5 Bed', '4 Bath', '4,070 Sq Ft', 'Built 2018'],
+    link: '/listings/Yarrow/Yarrow'
+  },
 
   {
-  slug: 'stillwater',
-  status: 'Residential Sale',
-  title: '6451 Still Water Court',
-  city: 'Midlothian, Texas 76065',
-  price: '$729,999',
-  image: '/listings/stillwater/stillwater-1.jpg',
-  gallery: Array.from(
-    { length: 15 },
-    (_, i) => `/listings/stillwater/stillwater-${i + 1}.jpg`
-  ),
-  facts: ['4 Bed', '3.5 Bath', '3,159 Sq Ft', 'Built 2016'],
-  link: '/listings/stillwater'
-},
-
+    slug: 'stillwater',
+    status: 'Residential Sale',
+    title: '6451 Still Water Court',
+    city: 'Midlothian, Texas 76065',
+    price: '$729,999',
+    image: '/listings/stillwater/stillwater-1.jpg',
+    gallery: Array.from(
+      { length: 15 },
+      (_, i) => `/listings/stillwater/stillwater-${i + 1}.jpg`
+    ),
+    facts: ['4 Bed', '3.5 Bath', '3,159 Sq Ft', 'Built 2016'],
+    link: '/listings/stillwater'
+  },
 
   {
     slug: '2935-shetland-drive',
@@ -99,17 +71,16 @@ export const listings = [
   },
 
   {
-   slug: '400-stonebrook-303',
+    slug: '400-stonebrook-303',
     status: 'Commercial Property',
     title: '400 Stonebrook Pkwy #303',
     city: 'Frisco, TX',
     price: '$429,999',
     image: '/listings/400-stonebrook-303/stonebrook-1.jpg',
     facts: ['4 Offices', '1 Bath', '1,225 Sq Ft', 'Suite #303'],
-    link: '/listings/400-stonebrook-303'
+    link: '/listings/400-stonebrook'
   },
 
-  
   {
     slug: '2900-galveston',
     status: 'Residential Lease',
@@ -120,8 +91,6 @@ export const listings = [
     facts: ['3 Bed', '2.5 Bath', '1,870 Sq Ft', 'Built 2018'],
     link: '/listings/2900-galveston'
   },
-
-
 
   {
     slug: '4414-silverweed-lane',
@@ -145,33 +114,33 @@ export const listings = [
     link: '/listings/1874-purtis-creek'
   },
 
-   {
-   slug: '400-stonebrook-301',
+  {
+    slug: '400-stonebrook-301',
     status: 'Commercial Lease',
     title: '400 Stonebrook Pkwy #301',
     city: 'Frisco, TX',
     price: '$3,299',
     image: '/listings/400-stonebrook-301/stone-1.jpg',
-     gallery: Array.from(
-    { length: 15 },
-    (_, i) => `/listings/400-stonebrook-301/stone-${i + 1}.jpg`
-  ),
+    gallery: Array.from(
+      { length: 15 },
+      (_, i) => `/listings/400-stonebrook-301/stone-${i + 1}.jpg`
+    ),
     facts: ['4 Offices', '1 Bath', '1,225 Sq Ft', 'Suite #301'],
-    link: '/listings/400-stonebrook-301'
+    link: '/listings/400-stonebrook-pkwy'
   },
-  
+
   {
-  slug: 'washington',
-  status: 'Residential lease',
-  title: '9929 George Washington Dr',
-  city: 'Mckinney, Texas, 75070',
-  price: '$2,999',
-  image: '/listings/washington/washington-1.jpg',
-  gallery: Array.from(
-    { length: 15 },
-    (_, i) => `/listings/washington/washington-${i + 1}.jpg`
-  ),
-  facts: ['3 Bed', '2 Bath', '1700 Sq Ft', 'Built 2005'],
-  link: '/listings/washington'
-}
-  ]
+    slug: 'washington',
+    status: 'Residential Lease',
+    title: '9929 George Washington Dr',
+    city: 'McKinney, Texas 75070',
+    price: '$2,999 / Month',
+    image: '/listings/washington/washington-1.jpg',
+    gallery: Array.from(
+      { length: 15 },
+      (_, i) => `/listings/washington/washington-${i + 1}.jpg`
+    ),
+    facts: ['3 Bed', '2 Bath', '1,700 Sq Ft', 'Built 2005'],
+    link: '/listings/washington'
+  }
+]
